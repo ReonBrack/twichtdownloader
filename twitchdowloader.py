e0790d0e62498eb79e0ccbb4b111b297f0c64ec3
ReonBrack <louagie.david@gmail.com>
2013-12-23 09:31:30 -0800

    added download option

--- twitchdowloader.py
+++ twitchdowloader.py
@@ -1,4 +1,5 @@
 # TODO
+# Fix download location
 # Overall progressbar
 # Addition Info of the VOD (eg. total size and length)
 # Skip the need to surf to "http://download.twitchapps.com" and make use of the parameters
@@ -8,6 +9,7 @@ import urllib2
 import sys
 import os
 import time
+from threading import Thread
 from progressbar import ProgressBar, Percentage, Bar
 from bs4 import BeautifulSoup 
 
@@ -16,6 +18,11 @@ path_base = os.path.expanduser("~/TwitchVideos/")
 output_path = ''
 
 #functions
+def intro():
+	print "Welcome to ReonBrack's TwitchVideo Downloader!"
+	print "Go to http://download.twitchapps.com/ type in the Channel Name, click view, click the download button next to the VOD you want to download en provide the script with the link to the popup window!"
+	print "This script will download all the parts for you in the location that you want!"
+
 def display_folders(): #TODO split function
 	list = os.listdir(path_base)
 	print "Choose a folder (0 - {})...".format(len(list))
@@ -28,8 +35,7 @@ def display_folders(): #TODO split function
 		new_folder(folder_name)
 	else:
 		output_path = path_base + list[option-1]
-		print "Folder {} selected...".format(output_path)
-		
+		print "Folder {} selected...".format(output_path)		
 		
 def new_folder(folder_name):
 	output_path = path_base + folder_name
@@ -46,7 +52,29 @@ def grab_links():
 		link_list.append(a['href'])
 	return link_list
 
-def download(list):
+def download_option():
+	print "Download options:"
+	print "1. Download all the parts at once"
+	print "2. Download the parts one by one"
+	option = input("Specify download option... ")
+	if option == 1 or option == 2:
+		return option
+	else:
+		download_option()
+		
+def download(list, option): 
+	if(option == 2):
+		download_one_by_one(list)
+	else:
+		download_all_at_once(list)
+		
+def download_all_at_once(list):
+	for video in list:
+		t = Thread(target = download_video, args = (video, ))
+		t.start()
+		t.join()
+
+def download_one_by_one(list):
 	i = 1
 	total = len(list)
 	for video in list:
@@ -72,19 +100,17 @@ def download_video(url):
 		
 		file_size_dl += len(buffer)
 		f.write(buffer)
-		time.sleep(0.01)
-		pbar.update(file_size_dl)
+		time.sleep(0.5)
+		pbar.update(file_size_dl+1)
 
 	f.close()
 	print ""
 
-def intro():
-	print "Welcome to ReonBrack's TwitchVideo Downloader!"
-	print "Go to http://download.twitchapps.com/ type in the Channel Name, click view, click the download button next to the VOD you want to download en provide the script with the link to the popup window!"
-	print "This script will download all the parts for you in the location that you want!"
+
 	
 #start
 intro()
 display_folders()
 list = grab_links()
-download(list)
+option = download_option()
+download(list, option)
